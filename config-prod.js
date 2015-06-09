fa85ca807c4eec2290c7e93297194389bd7ad5e1
Eemeli Kantola <eemeli.kantola@futurice.com>
2015-06-09 14:24:59 +0200

    Add QA and PROD configs to version control
    Now there are no more passwords needed

---
+++ config-prod.js
@@ -0,0 +1,10 @@
+exports.site = {
+    'googleAnalytics': {
+        'trackingID': 'UA-5362052-29'
+    },
+    'aws': {
+        'bucket': 'tech-prod.workplace.zalan.do',
+        'bucketPath': '',
+        'region': 'eu-central-1'
+    }
+};
---
+++ config-qa.js
@@ -0,0 +1,10 @@
+exports.site = {
+    'googleAnalytics': {
+        'trackingID': 'UA-5362052-33'
+    },
+    'aws': {
+        'bucket': 'tech-qa.workplace.zalan.do',
+        'bucketPath': '',
+        'region': 'eu-central-1'
+    }
+};
