ea2091c2651122448502ace2ec752c37bd3f33a9
Eemeli Kantola <eemeli.kantola@futurice.com>
2015-06-09 17:25:37 +0200

    Add dev config to repo as well

---
+++ config-dev.js
@@ -0,0 +1,10 @@
+exports.site = {
+    'googleAnalytics': {
+        'trackingID': 'UA-5362052-33'
+    },
+    'aws': {
+        'bucket': 'tech-dev.workplace.zalan.do',
+        'bucketPath': '',
+        'region': 'eu-central-1'
+    }
+};
